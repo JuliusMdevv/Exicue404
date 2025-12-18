c7c9eca0326b9ccc035175e61598186e9168ea8b
Julius Markwick <julius.markwick@gmail.com>
2025-12-18 13:51:04 -0800

    lets see

--- js/fetch.js
+++ js/fetch.js
@@ -1,8 +1,8 @@
-fetch('../data/info.json')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('output').textContent = document.getElementById('output').textContent.replace("Loading...", data.earlyaccess);
-    })
-    .catch(error => {
-        console.error('Error fetching the JSON data:', error);
-    });
+fetch('../../data/info.json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('output').textContent = data.earlyaccess;
+        })
+        .catch(err => {
+            console.error(err);
+        });
